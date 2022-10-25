28c04990a568395e16ed94d86307832c3c6c7949
Mohammad Alfayoume <mabed3819@gmail.com>
2022-10-25 15:32:39 +0300

    put the method inside the linkedList class

--- python/code_challenges/linkedlist/challenge01/test_challenge01.py
+++ python/code_challenges/linkedlist/challenge01/test_challenge01.py
@@ -1,122 +1,6 @@
 # Write your test here
 import pytest
-from challenge01 import Solution
-
-
-class Node:
-    def __init__(self,value):
-        self.value=value
-        self.next=None
-    
-class LinkedList:
-    def __init__(self):
-        self.head=None
-        self.tail=None
-        self.length=0
-    def append(self,value):
-        '''
-        input -> int or string
-
-        return -> None
-        '''
-        new_node= Node(value)
-        if self.head==None:
-            self.head=new_node
-        else:
-            current=self.head
-            while current.next != None:
-                current=current.next
-            current.next=new_node
-        self.length +=1
-
-    def get_node(self,value):
-        '''
-        input -> int or string
-
-        return -> object
-        '''
-        current=self.head
-        while current.value != value:
-            current=current.next
-        return current
-
-    def print_list_of_values(self):
-        '''
-        input -> None
-
-        return -> list
-        '''
-        values_in_list=[]
-        if self.head==None:
-            print("empty linked list")
-        else:
-            current=self.head
-            while current != None:
-                # print(current.value)
-                values_in_list.append(current.value)
-                current=current.next
-            print(self.length)
-        return values_in_list
-####################
-    def get_node_from_index(self,idx):
-        '''
-        input -> int
-
-        return -> object
-        '''
-        if self.head==None:
-            print("Empty LL")
-        else:
-            current= self.head
-            counter=0
-            while counter != idx:
-                current = current.next
-                counter +=1
-            return current
-    
-    def insert_node(self,idx,value):
-        '''
-        input -> int, int or string
-
-        return -> None
-        '''
-        new_node= Node(value)
-        if idx==0:
-            new_node.next=self.head
-            self.head=new_node
-        else:
-            next_node= self.get_node_from_index(idx)
-            new_node.next=next_node
-            prev_node= self.get_node_from_index(idx-1)
-            prev_node.next=new_node
-        self.length +=1
-
-    def delete_node(self,idx):
-        '''
-        input -> int
-
-        return -> None
-        '''
-        node_to_be_deleted= self.get_node_from_index(idx)
-        node_to_be_deleted.value=node_to_be_deleted.next.value
-        node_to_be_deleted.next= node_to_be_deleted.next.next
-        self.length -=1
-
-    def append_by_tail(self,value):
-        '''
-        input -> int or string
-
-        return -> None
-        '''
-        new_node= Node(value)
-        if self.head == None:
-            self.head=new_node
-            self.tail=self.head
-        else:
-            current=self.tail
-            current.next=new_node
-            self.tail=new_node
-####################
+from challenge01 import LinkedList
 
 ########### Test 01 ###########
 link1= LinkedList()
@@ -124,12 +8,6 @@ link1.append_by_tail(4)
 link1.append_by_tail(5)
 link1.append_by_tail(1)
 link1.append_by_tail(9)
-
-# print(link1.get_node_from_index(3))
-print(link1.print_list_of_values())
-# link1.delete_node(3)
-# link1.insert_node(0,8)
-# print(link1.print_list_of_values())
 
 def test_delete_node_one(one):
     '''
@@ -172,5 +50,5 @@ def one():
 
         return -> object
         '''
-    delete_node_method= Solution()
+    delete_node_method= LinkedList()
     return delete_node_method
--- python/code_challenges/linkedlist/challenge02/test_challenge02.py
+++ python/code_challenges/linkedlist/challenge02/test_challenge02.py
@@ -1,71 +1,14 @@
 # Write your test here
 import pytest
-from challenge02 import Solution
-class Node:
-    def __init__(self,value):
-        self.value=value
-        self.next=None
-    
-class LinkedList:
-    def __init__(self):
-        self.head=None
-        self.tail=None
-        self.length=0
-    
-    def get_node_from_index(self,idx):
-        '''
-        input -> int
+from challenge02 import LinkedList
 
-        return -> object
-        '''
-        if self.head==None:
-            print("Empty LL")
-        else:
-            current= self.head
-            counter=0
-            while counter != idx:
-                current = current.next
-                counter +=1
-            return current
-    
-
-    def append_by_tail(self,value):
-        '''
-        input -> int or string
-
-        return -> None
-        '''
-        new_node= Node(value)
-        if self.head == None:
-            self.head=new_node
-            self.tail=self.head
-        else:
-            current=self.tail
-            current.next=new_node
-            self.tail=new_node
-    
-    # def get_middle(self,head):
-    #     # 1 => 2 => 3 => 4 => 5
-    #     # 3 => 4 => 5
-    #     nodes=[]
-    #     current= head
-    #     # print(current)
-    #     while current != None:
-    #         nodes.append(current)
-    #         current= current.next
-    #     middle= len(nodes)//2
-    #     middle_node=[]
-    #     for i in range(middle,len(nodes)):
-    #         middle_node.append(nodes[i].value)
-    #     return middle_node
-
+########### Test 01 ###########
 link1= LinkedList()
 link1.append_by_tail(1)
 link1.append_by_tail(2)
 link1.append_by_tail(3)
 link1.append_by_tail(4)
 link1.append_by_tail(5)
-
 
 def test_get_middle_node_one(one):
     '''
@@ -79,6 +22,7 @@ def test_get_middle_node_one(one):
     actual= middle_node_values
     assert actual==expected
 
+########### Test 02 ###########
 
 link2= LinkedList()
 link2.append_by_tail(1)
@@ -101,6 +45,8 @@ def test_get_middle_node_two(one):
     actual= middle_node_values
     assert actual==expected
 
+########### Fixture Test ###########
+
 @pytest.fixture
 def one():
     '''
@@ -108,5 +54,5 @@ def one():
 
         return -> object
         '''
-    get_middle= Solution()
+    get_middle= LinkedList()
     return get_middle
