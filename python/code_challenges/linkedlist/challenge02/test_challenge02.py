ab5cc1c454a6b3c180078cb5c3cb3bf163df9c8c
Mohammad Alfayoume <mabed3819@gmail.com>
2022-10-25 17:06:50 +0300

    edit the test

--- python/code_challenges/linkedlist/challenge02/test_challenge02.py
+++ python/code_challenges/linkedlist/challenge02/test_challenge02.py
@@ -9,16 +9,18 @@ link1.append_by_tail(2)
 link1.append_by_tail(3)
 link1.append_by_tail(4)
 link1.append_by_tail(5)
+# x= link1.get_node_from_index(0)
+print(link1.get_middle())
 
-def test_get_middle_node_one(one):
+def test_get_middle_node_one():
     '''
         input -> object
 
         return -> boolean
         '''
-    expected=[3,4,5]
-    head= link1.get_node_from_index(0)
-    middle_node_values=one.get_middle(head)
+        #3 -> 4 -> 5
+    expected=3
+    middle_node_values=link1.get_middle()
     actual= middle_node_values
     assert actual==expected
 
@@ -31,28 +33,17 @@ link2.append_by_tail(3)
 link2.append_by_tail(4)
 link2.append_by_tail(5)
 link2.append_by_tail(6)
+print(link2.get_middle())
 
 
-def test_get_middle_node_two(one):
+
+def test_get_middle_node_two():
     '''
         input -> object
 
         return -> boolean
         '''
-    expected=[4,5,6]
-    head= link2.get_node_from_index(0)
-    middle_node_values=one.get_middle(head)
+    expected=4
+    middle_node_values=link2.get_middle()
     actual= middle_node_values
     assert actual==expected
-
-########### Fixture Test ###########
-
-@pytest.fixture
-def one():
-    '''
-        input -> None
-
-        return -> object
-        '''
-    get_middle= LinkedList()
-    return get_middle
