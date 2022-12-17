6f33135e482e2f8db9eca90ac460e9e82fecea88
Mohammad Alfayoume <mabed3819@gmail.com>
2022-12-17 22:58:21 +0200

    done

--- python/code_challenges/graph/challenge01/test_challenge01.py
+++ python/code_challenges/graph/challenge01/test_challenge01.py
@@ -1,1 +1,35 @@
 # Write your test here
+from .challenge01 import Graph
+
+g=Graph()
+
+g.addVertex("A")
+g.addVertex("B")
+g.addVertex("C")
+g.addVertex("D")
+g.addVertex("E")
+g.addVertex("F")
+g.addVertex("G")
+g.addVertex("H")
+g.addVertex("I")
+g.addVertex("K")
+
+
+g.addEdge("A", "B")
+g.addEdge("A", "C")
+g.addEdge("A","E")
+g.addEdge("B","D")
+g.addEdge("D","E")
+g.addEdge("C","F")
+g.addEdge("E","F")
+g.addEdge("E","G")
+g.addEdge("H","G")
+g.addEdge("H","F")
+g.addEdge("I","F")
+g.addEdge("I","K")
+g.addEdge("K","H")
+
+def test_01():
+    expected=['A', 'B', 'C', 'E', 'D', 'F', 'G', 'H', 'I', 'K']
+    actual =g.breadthFirst("A")
+    assert expected==actual
