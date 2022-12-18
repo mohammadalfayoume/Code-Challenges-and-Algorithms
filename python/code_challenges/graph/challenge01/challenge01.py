3bb96fcb83a55d53783a2bc4eec71d538ede7eba
Mohammad Alfayoume <mabed3819@gmail.com>
2022-12-18 02:31:56 +0200

    edit graph

--- python/code_challenges/graph/challenge01/challenge01.py
+++ python/code_challenges/graph/challenge01/challenge01.py
@@ -61,3 +61,33 @@ class Graph:
                     queue.append(neighbor)
         return result
             
+
+g=Graph()
+
+g.addVertex("A")
+g.addVertex("B")
+g.addVertex("C")
+g.addVertex("D")
+g.addVertex("E")
+g.addVertex("F")
+g.addVertex("G")
+g.addVertex("H")
+g.addVertex("I")
+g.addVertex("K")
+
+
+g.addEdge("A", "B")
+g.addEdge("A", "C")
+g.addEdge("A","E")
+g.addEdge("B","D")
+g.addEdge("C","F")
+g.addEdge("E","D")
+g.addEdge("E","F")
+g.addEdge("E","G")
+g.addEdge("F","H")
+g.addEdge("F","I")
+g.addEdge("G","H")
+g.addEdge("H","K")
+g.addEdge("I","K")
+
+print(g.breadthFirst("A"))
